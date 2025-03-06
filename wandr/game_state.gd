feb590c08f67a1c3f7c611f4ec76f9f353744502
Akka <anushckajoshi@gmail.com>
2025-03-05 23:48:07 -0500

    updated tilemaplayer for door

--- wandr/game_state.gd
+++ wandr/game_state.gd
@@ -1,12 +1,12 @@
 extends Node
-
 var num_levels = 2
-var current_level = 1
-var game_scene = 'res://Main/main.tscn'
+var current_level = 0
+var game_scene = "res://main.tscn"
+var level_one_scene = "res://levels/level_01.tscn"
 func restart():
-	current_level = 1
-	get_tree().change_scene_to_file('res://levels/level_01.tscn')
+	current_level = 0
+	get_tree().change_scene_to_file(level_one_scene)
 func next_level():
 	current_level += 1
 	if current_level <= num_levels:
-		get_tree().call_deferred("change_scene_to_file", game_scene)
+		get_tree().change_scene_to_file(game_scene)
--- wandr/level_base.gd
+++ wandr/level_base.gd
@@ -3,27 +3,24 @@ extends Node2D
 var door_scene = load("res://items/door.tscn")
 
 func _ready():
-	# Optionally hide the Items TileMap if you don't want it visible during gameplay.
-	# $Items.hide()
-	
-	# Reset the player’s position (and scale) using the SpawnPoint node
-	$player.reset($SpawnPoint.position)
-	
+	#$player.reset($SpawnPoint.position)
+	spawn_items()
 func spawn_items():
-	var item_cells = $Items.get_used_cells(0)
+	var item_cells = $Door.get_used_cells()
 	for cell in item_cells:
-		var data = $Items.get_cell_tile_data(0, cell)
+		var data = $Door.get_cell_tile_data(cell)
 		var type = data.get_custom_data("type")
 		if type == "door":
+			print("yesss")
 			var door = door_scene.instantiate()
 			add_child(door)
-			door.position = $Items.map_to_local(cell)
+			door.position = $Door.map_to_local(cell)
 			door.body_entered.connect(_on_door_entered)
 		#else:
 			##var item = item_scene.instantiate()
 			#add_child(item)
 			#item.init(type, $Items.map_to_local(cell))
-			#item.picked_up.connect(self._on_item_picked_up)
+			#item.picked_up.connect(self._on_item_picked_up)s
 
 func _on_door_entered(body):
 	GameState.next_level()
